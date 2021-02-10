de143ce91c0ab2738fd1e88f2bab832ef5b87803
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-02-10 19:42:08 +0700

    [UPDATE] add Like count and All item count

--- 62130500116_groupwork_3/main.js
+++ 62130500116_groupwork_3/main.js
@@ -4,21 +4,32 @@ const app = {
             task: 'Learing INT203 Client-side Web Programming 2',
             done: true,
             progress: 10,
-            users: [{username: "lnwza007",pic: './images/1.jpg',profile: './images/1.jpg',like:false},
-                     {username: "lalala_mc",pic: './images/2.jpg',profile: './images/2.jpg',like:false},
-                     {username: "Sylvia.silom",pic: './images/3.jpg',profile: './images/3.jpg',like:false}]
+            users: [{username: "lnwza007",pic: './images/1.jpg',profile: './images/1.jpg',like:false,count:279},
+                     {username: "lalala_mc",pic: './images/2.jpg',profile: './images/2.jpg',like:false,count:1708},
+                     {username: "Sylvia.silom",pic: './images/3.jpg',profile: './images/3.jpg',like:false,count:340}]
         }
         
     }
     ,
     methods: {
         toggleDone(index){
-            this.users[index].like = !this.users[index].like
+            // this.users[index].like = !this.users[index].like
+            if (this.users[index].like) {
+                this.users[index].like = !this.users[index].like
+                this.users[index].count -= 1
+            }else
+            {
+                this.users[index].like = !this.users[index].like
+                this.users[index].count += 1
+            }
         }
     },
     computed: {
-        countUndone(){
+        countLike(){
             return this.users.filter( i => i.like ).length
+        },
+        countPic(){
+            return this.users.filter( i => i).length
         }
     }
     
